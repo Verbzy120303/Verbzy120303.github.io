f70981b4b17c53fda8855f814ad39242325159b0
Verbzy120303 <kurakuratakibaka@gmail.com>
2023-12-15 11:12:24 +0800

    updated

--- vermarthreads/script.js
+++ vermarthreads/script.js
@@ -1,3 +1,17 @@
+function myFunction1() {
+  document.getElementById("Dropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.drop')) {
+  var myDropdown = document.getElementById("Dropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
